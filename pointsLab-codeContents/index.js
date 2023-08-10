12e14e0c62ed5eeb7137ac1136e15f42e7f42463
mikeyfennelly1 <mikeyp.fennelly@gmail.com>
2023-08-10 21:51:49 +0100

    tabs design in progress

--- pointsLab-codeContents/index.js
+++ pointsLab-codeContents/index.js
@@ -1,19 +1,26 @@
+const navbar = document.querySelector('topnav');
+const banner = document.querySelector('banner');
+const mainContent = document.querySelector('mainLandingContent');
+let landingHeight = navbar.offsetHeight + banner.offsetHeight + mainContent.offsetHeight;
+console.log(landingHeight);
+
+const heightAdjuster = document.querySelector('heightAdjuster');
+heightAdjuster.style.height = landingHeight + "px";
+
 const tabs = document.querySelectorAll('.tab_btn');
 const all_content = document.querySelectorAll('.content');
-
-var contentBox = document.querySelector('content');
-contentBox.style.top = mainLandingContent.offsetTop + 'px';
 
 tabs.forEach((tab, index)=> {
     tab.addEventListener('click', (e)=>{
         tabs.forEach(tab=>{tab.classList.remove('active')});
         tab.classList.add('active');
 
+        
         var line = document.querySelector('.line');
-        line.style.width = e.target.offsetHeight + "px"
-        line.style.left = e.target.offsetTop + "px"
-
-        all_content.forEach(content=>{content.classList.remove('active')});
+        line.style.height = e.target.offsetHeight + "px"
+        line.style.top = e.target.offsetTop + "px"
+        
+        all_content.forEach(content=>{content.classList.remove('active')})
         all_content[index].classList.add('active');
-    })
+})
 })

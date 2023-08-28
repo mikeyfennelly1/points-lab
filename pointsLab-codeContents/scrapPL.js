5eb4d53df73778b14a145b4293b8f3808f047087
mikeyfennelly1 <mikeyp.fennelly@gmail.com>
2023-08-28 19:43:46 +0100

    attempting to create the array logic needed for the carousel design

---
+++ pointsLab-codeContents/scrapPL.js
@@ -0,0 +1,19 @@
+let myArray = [0, 1, 2, 3];
+
+const left = document.getElementById('left');
+const right = document.getElementById('right');
+
+left.addEventListener('click', (e)=>{
+    let lastElement = myArray[myArray.length - 1];
+    console.log('lastElement: ' + lastElement);
+    myArray.pop(lastElement);
+    myArray.unshift(lastElement);
+    console.log(myArray);
+});
+right.addEventListener('click', (e)=>{
+    let firstElement = myArray[0];
+    console.log('firstElement: ' + firstElement);
+    
+    myArray.push(firstElement);
+    console.log(myArray);
+});

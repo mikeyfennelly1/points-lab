256fb6a9e59966313e0d389b54daa7e35ea83506
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2023-10-25 11:57:55 +0100

    boilerplate of subscriber API done

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,21 @@
 require('dotenv').config()
+
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose')
+const subscribersRouter = require('./routes/subscribers')
+const serverPort = 3004
 
 mongoose.connect(process.env.DATABASE_URL, {useNewURLParser: true});
 const db = mongoose.connection
 db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('Connected to Database'));
 
-app.listen(3004, () => console.log(`Server started on port ${process.env.DATABASE_URL}`))
+app.use(express.json())
+
+app.use('/subscribers', subscribersRouter)
+
+app.listen(serverPort, () => {
+    console.log(`Server started on port ${serverPort}`)
+    console.log(`Connecting to Mongo on port ${process.env.DATABASE_URL}`)
+})
